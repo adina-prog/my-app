21325e142d1a1beadbabc62173b673a64d2c5ae9
Adina Pervaiz <pervaizadina@gmail.com>
2022-04-19 12:49:03 +0400

    Updates

---
+++ precache-manifest.d28728c9c23255a5d34c7780a3cf666b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d00ba0b4b65705fc7aff",
+    "url": "/my-app/static/css/main.d9ca1843.chunk.css"
+  },
+  {
+    "revision": "d00ba0b4b65705fc7aff",
+    "url": "/my-app/static/js/main.d00ba0b4.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/my-app/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/my-app/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "de0023d09c0ef321d771",
+    "url": "/my-app/static/js/runtime~main.de0023d0.js"
+  },
+  {
+    "revision": "7fa8a5c4b3e68da747fc022b6ca59c33",
+    "url": "/my-app/index.html"
+  }
+];
